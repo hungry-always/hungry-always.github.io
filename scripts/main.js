417d4b4e975f39fcddca81cde203bc534c9d4365
胡林浩 <1540694044@qq.com>
2021-12-02 16:15:48 +0800

    优化界面

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,10 @@
-/*
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-        myImage.setAttribute('src', 'images/firefox-icon3.png');
-    } else {
-        myImage.setAttribute('src', 'images/firefox-icon.png');
-    }
-}
-*/
 let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
+let myHeading = document.querySelector('h3');
 if(!localStorage.getItem('name')) {
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+    myHeading.textContent = '欢迎来到 hungry-always.github.io，' + storedName;
 }
 myButton.onclick = function () {
     setUserName();
@@ -24,5 +12,5 @@ myButton.onclick = function () {
 function setUserName() {
     let myName = prompt('请输入你的名字。');
     localStorage.setItem('name', myName);
-    myHeading.textContent = "Mozilla 酷毙了" + myName;
+    myHeading.textContent = "欢迎来到 hungry-always.github.io，" + myName;
 }
